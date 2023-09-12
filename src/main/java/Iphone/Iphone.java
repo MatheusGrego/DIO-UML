7e12efa943508965bd0a214da65d7c5698f37452
MatheusGrego <matheusdeleongrego@gmail.com>
2023-09-12 11:09:03 -0300

    feat: iPhone class

---
+++ src/main/java/Iphone/Iphone.java
@@ -0,0 +1,48 @@
+package main.java.Iphone;
+
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadoInternet{
+    @Override
+    public void ligar(String numero) {
+
+    }
+
+    @Override
+    public void atender() {
+
+    }
+
+    @Override
+    public void iniciarConversaVoz() {
+
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+
+    }
+
+    @Override
+    public void atualizarPagina() {
+
+    }
+
+    @Override
+    public void tocar() {
+
+    }
+
+    @Override
+    public void pausar() {
+
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+
+    }
+}
